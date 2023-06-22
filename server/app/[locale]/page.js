7c49f8908518fe1f2a5319ddd3cda8bf310cca56
Kayron <setokayron@hotmail.com>
2023-06-22 20:50:11 -0300

    Updates

--- server/app/[locale]/page.js
+++ server/app/[locale]/page.js
@@ -858,12 +858,12 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 7510:
+/***/ 5277:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 125, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7844, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6249, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7844, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1522, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3100, 23))
 
